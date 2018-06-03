58dac835fb521dd1965506cbe29831904edd6f44
Jim Palowski <palowskijim@gmail.com>
2018-06-03 13:48:42 -0700

    Updated Packages

--- spec/galactic-age-calculator-spec.js
+++ spec/galactic-age-calculator-spec.js
@@ -3,7 +3,7 @@ import { GalacticAgeCalculator } from './../src/galatic-age-calculator.js';
 describe ('galatic-age-checker', function() {
   it ('should test whether the users age is in Mercury Age', function() {
     var galaticAge = new GalacticAgeCalculator(12);
-    console.log(haiku);
+    console.log(galaticAge);
     expect(galaticAge.checkGalaticAge(12)).toEqual(12);
     console.log("new " + checkGalaticAge());
   });
